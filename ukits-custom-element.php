b9488a121d62253accdeb3b2477e636222c1ed8d
gaziakter <hellogaziakter@gmail.com>
2026-07-23 07:48:49 +0600

    hero description style update

--- ukits-custom-element.php
+++ ukits-custom-element.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: UKITS Custom Element
  * Description: Elementor custom widgets generated from the UKITS HTML template sections.
- * Version: 1.4.0
+ * Version: 1.4.1
  * Author: Gazi Akter
  * Author URI: https://gaziakter.com/
  * Text Domain: ukits-custom-element
@@ -18,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'UKITS_CUSTOM_ELEMENT_VERSION', '1.4.0' );
+define( 'UKITS_CUSTOM_ELEMENT_VERSION', '1.4.1' );
 define( 'UKITS_CUSTOM_ELEMENT_FILE', __FILE__ );
 define( 'UKITS_CUSTOM_ELEMENT_PATH', plugin_dir_path( __FILE__ ) );
 define( 'UKITS_CUSTOM_ELEMENT_URL', plugin_dir_url( __FILE__ ) );
